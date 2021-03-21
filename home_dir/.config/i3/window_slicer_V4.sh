9b4d1041005f65db6c8bdaea68f8c300c874ee82
Ivan Milov <522518+ivanmilov@users.noreply.github.com>
2021-03-21 09:59:55 +0100

    [i3] window slicer just creates a placeholder for new window

--- home_dir/.config/i3/window_slicer_V4.sh
+++ home_dir/.config/i3/window_slicer_V4.sh
@@ -6,24 +6,25 @@ window_x=${window_x:=0}
 window_y=${window_y:=0}
 window_width=${window_width:=2}
 window_height=${window_height:=2}
-instance=${instance:=$TERM}
-window_count=$(i3-msg -t get_tree | jq -r '.. | .nodes?[]? | select(.window_type == "normal") | .name' | wc -l)
-# getting cursor coordinates
-eval $(xdotool getmouselocation --shell)
+# instance=${instance:=$TERM}
+# window_count=$(i3-msg -t get_tree | jq -r '.. | .nodes?[]? | select(.window_type == "normal") | .name' | wc -l)
+# getting cursor coordinates and select window under mouse cursor
+eval $(xdotool getmouselocation --shell mousedown 1 mouseup 1)
 # getting window coordinates and dimentions
 eval $( i3-msg -t get_tree | jq -r '..|try select(.focused == true)| "window_x=\(.rect.x)\nwindow_y=\(.rect.y)\nwindow_width=\(.rect.width)\nwindow_height=\(.rect.height)\ninstance=\(.window_properties.instance)"' )
 
-waitloop(){
-    until [[ $window_count -lt $new_window_count ]];
-    do
-        new_window_count=$(i3-msg -t get_tree | jq -r '.. | .nodes?[]? | select(.window_type == "normal") | .name' | wc -l)
-        sleep 0.1
-    done
-}
+# waitloop(){
+#     until [[ $window_count -lt $new_window_count ]];
+#     do
+#         new_window_count=$(i3-msg -t get_tree | jq -r '.. | .nodes?[]? | select(.window_type == "normal") | .name' | wc -l)
+#         sleep 0.1
+#     done
+# }
 
 slicing(){
     change=$(( $1 / 2 - ($2 - $3) ))
-    i3-msg split "$5" && alacritty & disown && waitloop
+    # i3-msg split "$5" && alacritty & disown && waitloop
+    i3-msg split "$5" && i3-msg open
     if [ "$change" -gt 0 ]; then
         i3-msg resize grow "$4" "$change"
     else
