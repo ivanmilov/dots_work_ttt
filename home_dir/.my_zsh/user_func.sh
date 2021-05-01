51b6f06309d02da454a42780b687094363587240
Ivan Milov <522518+ivanmilov@users.noreply.github.com>
2021-05-01 19:57:30 +0200

    [zsh] kde_set_wall

--- home_dir/.my_zsh/user_func.sh
+++ home_dir/.my_zsh/user_func.sh
@@ -13,3 +13,31 @@ my_get_active_player() {
 my_print_my_zsh_aliases() {
 	bat $MY_ZSH/aliases.zsh | fzf
 }
+
+kde_set_wall()
+{
+dbus-send --session --dest=org.kde.plasmashell --type=method_call /PlasmaShell org.kde.PlasmaShell.evaluateScript 'string:
+var Desktops = desktops();
+for (i=0;i<Desktops.length;i++) {
+        d = Desktops[i];
+        d.wallpaperPlugin = "org.kde.image";
+        d.currentConfigGroup = Array("Wallpaper",
+                                    "org.kde.image",
+                                    "General");
+        d.writeConfig("Image", "file://'"$1"'");
+}'
+}
+
+kde_set_wall_url()
+{
+dbus-send --session --dest=org.kde.plasmashell --type=method_call /PlasmaShell org.kde.PlasmaShell.evaluateScript 'string:
+var Desktops = desktops();
+for (i=0;i<Desktops.length;i++) {
+        d = Desktops[i];
+        d.wallpaperPlugin = "org.kde.image";
+        d.currentConfigGroup = Array("Wallpaper",
+                                    "org.kde.image",
+                                    "General");
+        d.writeConfig("Image", "'"$1"'");
+}'
+}
