c7bbd55fd8bd2a7a016bee790683218bc325ac1d
Ivan Milov <522518+ivanmilov@users.noreply.github.com>
2021-05-20 16:24:33 +0200

    [bspwm] modeules bspwm and spotify_remote (WIP)

--- home_dir/.config/polybar/my/launch.sh
+++ home_dir/.config/polybar/my/launch.sh
@@ -10,6 +10,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+
 # Launch the bar
-polybar -q left -c "$DIR"/config.ini &
-polybar -q right -c "$DIR"/config.ini &
+if [[ $1 == "debug" ]]; then
+    polybar -l trace left -c "$DIR"/config.ini &
+    polybar -l trace right -c "$DIR"/config.ini &
+elif [[ $1 == "one" ]]; then
+    polybar left right -c "$DIR"/config.ini &
+else
+    polybar -q left -c "$DIR"/config.ini &
+    polybar -q right -c "$DIR"/config.ini &
+fi
