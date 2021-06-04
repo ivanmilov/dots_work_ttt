f8505baa8e07d17368a8b19e24961593cc1e7b2e
Ivan Milov <522518+ivanmilov@users.noreply.github.com>
2021-06-04 20:44:21 +0200

    [bspwm] remove_unplugged_monitors true

--- home_dir/.config/bspwm/scripts/swap_desktop_to_another_monitor.sh
+++ home_dir/.config/bspwm/scripts/swap_desktop_to_another_monitor.sh
@@ -5,6 +5,8 @@ n=0
 # save current desktop
 curr_desk_id=$(bspc query -D -d)
 
+first=$(bspc query -M --names | head -n1)
+
 # get focused monitor
 while read -r line
 do
@@ -14,8 +16,11 @@ do
     n=$(($n + 1))
 done < <(bspc query -M)
 
-mon_id=$( bspc query -M -m focused )
-[[ $mon_id == $m0 ]] && mon_id=$m1
+# if there are only 2 mons, shift them
+[[ ! $m2 ]] && m2=$m1; m1=$m0;
+
+mon_id=$( bspc query -M -m focused)
+[[ $mon_id == $m0 && $first == "DP-0" ]] && mon_id=$m1;
 
 # get target monitor
 [[ $mon_id == $m1 ]] && target_mon=$m2 || target_mon=$m1
