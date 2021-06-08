da3133b275f1bc2ee54262be985ffc33279113cc
Ivan Milov <522518+ivanmilov@users.noreply.github.com>
2021-06-08 21:24:40 +0200

    [bin] use exported mon_id in 'swap_desktop_to_another_monitor'

--- home_dir/.config/bspwm/scripts/swap_desktop_to_another_monitor.sh
+++ home_dir/.config/bspwm/scripts/swap_desktop_to_another_monitor.sh
@@ -2,28 +2,17 @@
 
 n=0
 
+source export_mons.sh
+
 # save current desktop
 curr_desk_id=$(bspc query -D -d)
 
 first=$(bspc query -M --names | head -n1)
 
-# get focused monitor
-while read -r line
-do
-    [[  $n == 0 ]] && m0="$line"
-    [[  $n == 1 ]] && m1="$line"
-    [[  $n == 2 ]] && m2="$line"
-    n=$(($n + 1))
-done < <(bspc query -M)
-
-# if there are only 2 mons, shift them
-[[ ! $m2 ]] && m2=$m1; m1=$m0;
-
 mon_id=$( bspc query -M -m focused)
-[[ $mon_id == $m0 && $first == "DP-0" ]] && mon_id=$m1;
 
 # get target monitor
-[[ $mon_id == $m1 ]] && target_mon=$m2 || target_mon=$m1
+[[ $mon_id == $m1_id ]] && target_mon=$m2_id || target_mon=$m1_id
 
 # focus target monitor
 bspc monitor -f $target_mon
