24a804bcf2bd475afe8dd029696804c20994f8b3
Ivan Milov <522518+ivanmilov@users.noreply.github.com>
2021-07-04 10:37:04 +0200

    [bspwm] export mons sorted

--- home_dir/.config/bspwm/scripts/export_mons.sh
+++ home_dir/.config/bspwm/scripts/export_mons.sh
@@ -5,5 +5,5 @@ mon_num=`bspc query -M | wc -l`
 export m1=$(( mon_num - 1 ))
 export m2=$mon_num
 
-export m1_id=`bspc query -M | sed -n '1 p' `
-export m2_id=`bspc query -M | sed -n '2 p' `
+export m1_id=`bspc query -M | sort | sed -n '1 p' `
+export m2_id=`bspc query -M | sort | sed -n '2 p' `
